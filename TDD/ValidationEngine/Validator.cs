6882ebdcc3950fb8015573d1edbf12286b5747d8
7acc <jacc321@gmail.com>
2017-05-23 14:31:23 +0200

    testclass and validatorclass

---
+++ TDD/ValidationEngine/Validator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationEngine
+{
+    public class Validator
+    {
+
+        public bool ValidateEmailAddress(string emailAdress)
+        {
+            
+            return false;
+        }
+    }
+}
---
+++ TDD/ValidationEngineTests/ValidationTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ValidationEngine;
+
+namespace ValidationEngineTests
+{
+    [TestFixture]
+    public class ValidationTests
+    {
+        [Test]
+        void TrueForValidAdress()
+        {
+            var sut = new Validator();
+      
+            Assert.IsTrue(sut.ValidateEmailAddress("Jacc@email.com"));
+
+        }
+
+
+    }
+}
