7f4ead9d76c6abf34b45927c72269bf9b2d4dd14
7acc <jacc321@gmail.com>
2017-05-24 18:05:09 +0200

    started on exercise 2

---
+++ TDD/TravelAgency/TourSchedule.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency
+{
+   public class TourSchedule
+   {
+       private List<Tour> Tours;
+
+       public TourSchedule()
+       {
+           Tours = new List<Tour>();
+       }
+
+       public void CreateTour(string description, DateTime date, int nbrOfSeats)
+       {
+           if (TourDateAvailable(date))
+           {
+               Tours.Add(new Tour
+               {
+                   Description = description,
+                   TourDate = date.Date,
+                   NbrOfSeats = nbrOfSeats
+               });
+           }
+           else
+           {
+               //throw exception!
+           }
+       }
+
+     
+
+       public List<Tour> GetToursFor(DateTime tourDate)
+       {
+           return Tours.Where(x => x.TourDate.Date == tourDate.Date).ToList();
+       }
+
+
+
+
+        //------------------------------------------------HELPERS--------------------------------------------------------
+        public bool TourDateAvailable(DateTime date)
+        {
+            return Tours.Count(x => x.TourDate.Date == date.Date) < 3;
+        }
+    }
+
+    public class Tour
+    {
+        public DateTime TourDate { get; set; }
+        public string Description { get; set; }
+        public int NbrOfSeats { get; set; }
+    }
+}
---
+++ TDD/TravelAgencyTests/TourScheduleTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Internal;
+using NUnit.Framework;
+using TravelAgency;
+
+namespace TravelAgencyTests
+{
+    [TestFixture]
+    class TourScheduleTests
+    {
+        private TourSchedule sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            sut = new TourSchedule();
+
+        }
+
+        [Test]
+        public void CanCreateNewTour()
+        {
+            var tourDate = new DateTime(2017, 8, 10,1,1,1);
+
+            sut.CreateTour("Best tour ever",
+                tourDate, 22);
+
+            var tours = sut.GetToursFor(tourDate);
+            var tour = tours[0];
+
+            Assert.True(tours.Count == 1);    
+                
+            Assert.True(
+                tour.TourDate == tourDate.Date
+                && tour.NbrOfSeats == 22 
+                && tour.Description == "Best tour ever");
+        }      
+    }
+}
